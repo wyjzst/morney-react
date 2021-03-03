eb57868171051785c694000ad179d91f95ba6341
一只伊布 <wyjzst@126.com>
2021-03-03 13:11:33 +0800

    使图标也能点击,修复样式冲突

---
+++ src/components/Nav.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import React from 'react';
+import Icon from './Icon';
+
+const NavWrapper = styled.nav`
+  line-height: 24px;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  > ul {
+    display:flex;
+    > li{
+      width: 33.3333%;
+      text-align:center;
+      > a {
+        display: flex;
+        flex-direction: column;
+        padding: 4px 0;
+        justify-content: center;
+        align-items: center;
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
+    }
+  }
+`;
+
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/tags">
+            <Icon name="tag"/>
+            标签页
+          </Link>
+        </li>
+        <li>
+          <Link to="/money">
+            <Icon name="money"/>
+            记账页
+          </Link>
+        </li>
+        <li>
+          <Link to="/statistics">
+            <Icon name="chart"/>
+            统计页
+          </Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+};
+
+export default Nav;
