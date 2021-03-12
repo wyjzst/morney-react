47a924809e05081520201b5fba8fff2a9557aff9
一只伊布 <wyjzst@126.com>
2021-03-12 20:07:39 +0800

    完成备注功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
-import {NotesSection} from './Money/NotesSection';
+import {NoteSection} from './Money/NoteSection';
 import {CategorySection} from './Money/CategorySection';
 import {NumberPadSection} from './Money/ NumberPadSection';
 import {TagsSection} from './Money/TagsSection ';
@@ -15,12 +15,7 @@ function Money() {
     <MyLayout>
 
       <TagsSection/>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里添加备注"/>
-        </label>
-      </NotesSection>
+      <NoteSection/>
       <CategorySection>
         <ul>
           <li className="selected">支出</li>
